8de18e03a7d218f91a8e1bb9d0c194651d0d8586
Emiliano Polania <81882991+serinsispc@users.noreply.github.com>
2026-01-25 12:07:51 -0500

    Modernize client table styling

---
+++ DAL/Controler/type_regimesControler.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Controler
+{
+    public class type_regimesControler
+    {
+        public static async Task<List<type_regimes>> ListaTiposRegimen(string db)
+        {
+            try
+            {
+                var cn = new SqlAutoDAL();
+                var resp = await cn.ConsultarLista<type_regimes>(db);
+                return resp.ToList();
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                return new List<type_regimes>();
+            }
+        }
+    }
+}
