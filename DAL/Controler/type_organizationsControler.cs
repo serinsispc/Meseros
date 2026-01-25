60c93990acfada552078183dba5aa7241827f066
Emiliano Polania <81882991+serinsispc@users.noreply.github.com>
2026-01-25 11:13:29 -0500

    Load municipal and tax lists for customer modal

---
+++ DAL/Controler/type_organizationsControler.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Controler
+{
+    public class type_organizationsControler
+    {
+        public static async Task<List<type_organizations>> ListaTiposOrganizacion(string db)
+        {
+            try
+            {
+                var cn = new SqlAutoDAL();
+                var resp = await cn.ConsultarLista<type_organizations>(db);
+                return resp.ToList();
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                return new List<type_organizations>();
+            }
+        }
+    }
+}
---
+++ DAL/Model/type_organizations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class type_organizations
+    {
+        public int? id { get; set; }
+        public string name { get; set; }
+    }
+}
