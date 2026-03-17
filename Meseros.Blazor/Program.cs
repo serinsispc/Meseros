d5b319d43415c58f6e3338f45be3f1f9c924efcf
Emiliano Polania <81882991+serinsispc@users.noreply.github.com>
2026-03-16 22:30:18 -0500

    16-03-2026    22.30

--- Meseros.Blazor/Program.cs
+++ Meseros.Blazor/Program.cs
@@ -1,9 +1,14 @@
 using Meseros.Blazor.Components;
+using Meseros.Blazor.Models;
+using Meseros.Blazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<LoginDataService>();
+builder.Services.AddScoped<OperationalContextStore>();
 
 var app = builder.Build();
 
